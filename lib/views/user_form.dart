afb8f52dead16f54a65ecfcae9be5ec8a8285414
NatanFiuza <natan.fiuza@gmail.com>
2023-03-05 20:45:47 -0300

    Adicionando validate / EmailValidator package

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -1,16 +1,70 @@
-
 import 'package:flutter/widgets.dart';
-import 'package:provider/single_child_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class UserForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final isValid = _form.currentState!.validate();
+
+              if (isValid) {
+                _form.currentState!.save();
+                Navigator.of(context).pop();
+              }
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: _form,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Favor informar um nome';
+                  }
+                  if(value.trim().length <= 3) {
+                    return 'Nome muito curto';
+                  }
+                  return null;
+                
+                },
+                onSaved: (value) {
+                  print(value);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-mail'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Favor informar um e-mail';
+                  }
+                  if (!EmailValidator.validate(value)) {
+                      return 'Informe um e-mail válido';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'URL do avatar'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
-  
 }
